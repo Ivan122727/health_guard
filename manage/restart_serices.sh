c382eedcc359744aa28e482acb163fedbc85a34c
Ivan122727 <ermolovivan2018@gmail.com>
2025-04-28 00:49:50 +0500

    upd

---
+++ manage/restart_serices.sh
@@ -0,0 +1,6 @@
+sudo systemctl stop tgbot
+sudo systemctl stop tgbot_worker
+sudo systemctl stop admin_panel
+sudo systemctl start tgbot
+sudo systemctl start tgbot_worker
+sudo systemctl start admin_panel
