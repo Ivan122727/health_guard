a63263592c81b56201ae936e33bcbbfb6afecbee
Ivan122727 <ermolovivan2018@gmail.com>
2025-06-17 13:42:06 +0300

    res

--- shared/sqladmin_/model_view/user.py
+++ shared/sqladmin_/model_view/user.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from wtforms import SelectField
 
 from shared.sqladmin_.model_view.common import SimpleMV
 from shared.sqlalchemy_db_.sqlalchemy_model import UserDBM
@@ -25,7 +26,15 @@ class UserMV(SimpleMV, model=UserDBM):
         UserDBM.role,
         UserDBM.is_active,
     ]
-
+    form_overrides = {
+        UserDBM.role.key: SelectField
+    }
+    form_args = {
+        UserDBM.role.key: {
+            "choices": [(role, role) for role in UserDBM.Roles.to_set()],
+            "description": "Выберите роль пользователя"
+        }
+    }
     column_sortable_list = sqlalchemy.inspect(UserDBM).columns
     column_default_sort = [
         (UserDBM.creation_dt, True)
